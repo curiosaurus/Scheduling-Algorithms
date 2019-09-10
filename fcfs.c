48fa7507424f19d88bd115a6847631ae5e3450d9
Pratik Patil <35042555+curiosaurus@users.noreply.github.com>
2019-09-10 12:19:25 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,27 +1,20 @@
- 
 #include<stdio.h>  
-  
-void findWaitingTime(int processes[], int n, int bt[], int wt[])  
-{  
+void findWaitingTime(int processes[], int n, int bt[], int wt[])  {  
     wt[0] = 0;  
     for (int  i = 1; i < n ; i++ )  
         wt[i] =  bt[i-1] + wt[i-1] ;  
 }  
-void findTurnAroundTime( int processes[], int n,   
-                  int bt[], int wt[], int tat[])  
-{  
+void findTurnAroundTime( int processes[], int n,int bt[], int wt[], int tat[])  {  
     for (int  i = 0; i < n ; i++)  
         tat[i] = bt[i] + wt[i];  
 }  
     
-void findavgTime( int processes[], int n, int bt[])  
-{  
+void findavgTime( int processes[], int n, int bt[]){  
     int wt[n], tat[n], total_wt = 0, total_tat = 0;  
     findWaitingTime(processes, n, bt, wt); 
     findTurnAroundTime(processes, n, bt, wt, tat);  
     printf("Processes   Burst time   Waiting time   Turn around time\n");  
-    for (int  i=0; i<n; i++)  
-    {  
+    for (int  i=0; i<n; i++){  
         total_wt = total_wt + wt[i];  
         total_tat = total_tat + tat[i];  
         printf("   %d ",(i+1)); 
